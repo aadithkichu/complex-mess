2bb192dfdd5c22424adcc7da11f8352bce61a4ed
aadithkichu <aadithkichu123@gmail.com>
2025-11-03 23:56:25 +0530

    feat: pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css'; // <-- This line is CRUCIAL
 import './index.css'
+import { registerSW } from 'virtual:pwa-register';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {}
+});
